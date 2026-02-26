55a6cf6ac1b64669b24986f5ee0a93745ac5f103
Sebastian Brunner <brunner.sebi4@gmail.com>
2026-02-26 09:58:29 +0100

    remove tasks & make lead fields optional

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,18 +2,14 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
-    tasks: defineTable({
-        text: v.string(),
-        isCompleted: v.boolean(),
-    }),
     leads: defineTable({
         title: v.string(),
-        description: v.string(),
-        owner: v.string(),
-        category: v.string(),
-        issue: v.object({
+        description: v.optional(v.string()),
+        owner: v.optional(v.string()),
+        category: v.optional(v.string()),
+        issue: v.optional(v.object({
             year: v.number(),
             number: v.number(),
-        }),
+        })),
     }),
 });
