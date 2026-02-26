9878e11f8b2ec5e79ed271faace080c31da6b0ad
Sebastian Brunner <brunner.sebi4@gmail.com>
2026-02-26 12:16:10 +0100

    create leads mutation

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,15 +1,15 @@
-import { defineSchema, defineTable } from "convex/server";
-import { v } from "convex/values";
+import { defineSchema, defineTable } from 'convex/server'
+import { v } from 'convex/values'
 
 export default defineSchema({
-    leads: defineTable({
-        title: v.string(),
-        description: v.optional(v.string()),
-        owner: v.optional(v.string()),
-        category: v.optional(v.string()),
-        issue: v.optional(v.object({
-            year: v.number(),
-            number: v.number(),
-        })),
-    }),
-});
+  leads: defineTable({
+    title: v.string(),
+    description: v.optional(v.string()),
+    owner: v.optional(v.string()),
+    category: v.optional(v.string()),
+    issue: v.optional(v.object({
+      year: v.number(),
+      number: v.number()
+    }))
+  })
+})
